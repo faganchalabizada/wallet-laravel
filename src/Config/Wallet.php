b5083a1200416f55c99fc023ca14c2e354dc4874
Feqan <feqan@celebizade.info>
2018-10-24 14:18:27 +0400

    ..

--- src/Config/Wallet.php
+++ src/Config/Wallet.php
@@ -5,5 +5,5 @@ return [
     'account_types_table' => 'wl_account_types',
     'transaction_types_table' => 'wl_transaction_types',
     'user_total_balance_table' => "wl_user_total_balance",
-    'user_tsotal_balance_table' => "wl_user_total_balance",
+    'test' => "test",
 ];
